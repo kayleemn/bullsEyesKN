c133591e97e80e1530731e08aeeb81e971082198
Kaylee Nieves <kayleenieves@Kaylees-MacBook-Pro.local>
2019-03-15 09:37:40 -0400

    Bullseyes window created

---
+++ bullsEyesKN01.py
@@ -0,0 +1,12 @@
+#bullsEyes by Kaylee
+
+from graphics import*
+
+cirSz = 50
+
+bullsWin = GraphWin("Bullseyes!", cirSz * 10, cirSz * 10)
+bullsWin.setCoords(0, 0, cirSz * 10, cirSz * 10)
+
+
+bullsWin.getMouse()
+bullsWin.close()

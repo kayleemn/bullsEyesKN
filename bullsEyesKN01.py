2a4dec84f9259c7aec5df347208b9f0b06b44ea5
Kaylee Nieves <kayleenieves@Kaylees-MacBook-Pro.local>
2019-03-15 13:25:21 -0400

    blue circle added in the middle of the screen

--- bullsEyesKN01.py
+++ bullsEyesKN01.py
@@ -2,11 +2,20 @@
 
 from graphics import*
 
+def draw_cir(Cx, Cy, size, color, bullWin):
+    circle = circle(Point(Cx, Cy),Point(Cx, Cy))
+    circle.setFill(color)
+    circle.draw(bullWin)
+
 cirSz = 50
+cirCol = "blue"
 
-bullsWin = GraphWin("Bullseyes!", cirSz * 10, cirSz * 10)
-bullsWin.setCoords(0, 0, cirSz * 10, cirSz * 10)
+bullWin = GraphWin("Bullseye!", cirSz * 10, cirSz * 10)
+bullWin.setCoords(0, 0, 900, 900)
 
+bCircle = Circle(Point(450,450), 25.5)
+bCircle.setFill(color_rgb(10,10,235))
+bCircle.draw(bullWin)
 
-bullsWin.getMouse()
-bullsWin.close()
+bullWin.getMouse()
+bullWin.close()
